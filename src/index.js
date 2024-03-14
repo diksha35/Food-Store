84d3b97a1e181d6b80c2fd052242fa03d15f67ee
diksha35 <deshmukhdiksha08@gmail.com>
2024-03-14 19:13:38 +0530

    routing is added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-;
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { createBrowserRouter,Outlet,RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 /**
  * Header
  * -Logo
@@ -18,23 +20,39 @@ import Footer from "./components/Footer";
  *      -Name of Res, Star Rating
  * Footer-
  * -copyright
- * -Links 
+ * -Links
  * -address
  *- contact
  */
 
+ 
 
-
-const AppLayout=()=>{
-    return (<>
-    <div className="app">
+const AppLayout = () => {
+  return (
+    <>
+      <div className="app">
         <Header></Header>
-        <Body></Body>
+        <Outlet></Outlet>
         <Footer></Footer>
-    </div>
-    </>)
-}
+      </div>
+    </>
+  );
+};
 
+const appRouter = createBrowserRouter([
+    { path: "/", element: <AppLayout /> , 
+    children :[
+        {
+          path: "/", element : <Body/>
+        },
+      { 
+        path:"/about", element :<About/>
+      },
+      {path:"/contact-us", element:<Contact/>}
+    ]
+    ,errorElement:<Error/>},
+   
+  ]);
 
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(<AppLayout />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter}></RouterProvider>)
