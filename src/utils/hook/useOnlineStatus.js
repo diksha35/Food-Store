4a27d973e90f0be1f3baea4f066089c0baeef5f5
diksha35 <deshmukhdiksha08@gmail.com>
2024-03-27 23:44:09 +0530

    create a folder for custom hook & placed file inside this folder

---
+++ src/utils/hook/useOnlineStatus.js
@@ -0,0 +1,21 @@
+import { useState,useEffect } from "react";
+
+
+const useOnlineStatus=()=>{
+    const [status, setStatus]=useState(true);
+    
+   useEffect(()=>{
+    window.addEventListener("offline",(event)=>{
+        setStatus(false);
+    });
+
+    window.addEventListener("online",(event)=>{
+        setStatus(true);
+    })
+   },[]);
+
+
+    return status;
+}
+
+export default useOnlineStatus;
